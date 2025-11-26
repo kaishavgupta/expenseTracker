d386aded0e0cd9452e161110a1790f8439792e70
kaishavgupta <kaishavgupta4.2001@gmail.com>
2025-11-27 02:26:44 +0530

    dotenv config update

--- server/server.js
+++ server/server.js
@@ -1,35 +1,36 @@
 import cors from "cors";
-import express from "express"
-import {authRouter} from "./routes/auth-Router.js"
-import { connectDb } from "./utils/db.js"
+import express from "express";
+import { authRouter } from "./routes/auth-Router.js";
+import { connectDb } from "./utils/db.js";
 import { errorMiddleware } from "./middlewares/error-middleware.js";
 import { transactinRoute } from "./routes/transaction-Router.js";
 import cookieParser from "cookie-parser";
+import "dotenv/config";
 
+const app = express();
 
-const app= express()
-
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.use(cors({
   origin: "http://localhost:5174",
   credentials: true
 }));
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/",authRouter)
-app.use("/",transactinRoute)
+app.use("/", authRouter);
+app.use("/", transactinRoute);
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-const PORT =3000
+const PORT = process.env.PORT; // ✔ use env
 
-connectDb().then(()=>{
-    app.listen(PORT,()=>{
-        console.log(`Server is running on ${PORT}`);
-    })
-})
-.catch(()=>{
+connectDb()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
+    });
+  })
+  .catch(() => {
     console.log("Something went wrong");
-})
+  });
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-dotenv.config();    // <-- FIX
 
-const MONGODB_URI=process.env.MONGODB_URI
-
+const MONGODB_URI = process.env.MONGODB_URI;
 
 export const connectDb = async () => {
   try {
     await mongoose.connect(MONGODB_URI);
     console.log("DB Connected Successfully");
   } catch (error) {
-    console.log("DB connection failed");
+    console.log("DB connection failed", error);
     process.exit(0);
   }
 };
